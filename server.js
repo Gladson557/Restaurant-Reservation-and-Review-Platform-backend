8e8ca2f4c52efe3fb2baec05f8d66fb5a8aac5b9
Gladson <gladsonbright592@gmail.com>
2025-09-11 11:55:35 +0530

    backend

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
